94d2d37edd250147788d8a3e7a7ab15b0d5e0d24
Svyatoslav Trostynskyi <svyatoslavtrostynski24@gmail.com>
2019-11-18 23:52:28 +0200

    create login part of application

---
+++ LoginPart/login_activity2/app/src/main/java/com/example/login_activity/Second.java
@@ -0,0 +1,16 @@
+package com.example.login_activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.login_activity.R;
+
+
+public class Second extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
