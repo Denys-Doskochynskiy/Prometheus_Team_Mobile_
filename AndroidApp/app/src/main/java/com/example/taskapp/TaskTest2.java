083bf1ace8f1811ef42d1547e0ca48a160b8dbb4
Prometheus-Mobile <doskochynska83@gmail.com>
2019-11-18 21:14:31 +0200

    Adding the task-skrin from app

---
+++ AndroidApp/app/src/main/java/com/example/taskapp/TaskTest2.java
@@ -0,0 +1,19 @@
+package com.example.taskapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TaskTest2 extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_test2);
+
+    }
+
+}
